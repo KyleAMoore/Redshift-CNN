3fa37f86a19aeeea04e1c588e1fcee16b70ab3c9
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2020-04-20 22:46:46 -0500

    Provide click options in submit_preprocessing_job. (Closes #24) (#26)

--- preprocessing/submit_preprocessing_job.py
+++ preprocessing/submit_preprocessing_job.py
@@ -11,6 +11,8 @@ import logging
 import tempfile
 import os
 
+import click
+
 from SciServer.Authentication import login
 import SciServer.Files as sf
 import SciServer.Jobs as sj
@@ -74,11 +76,11 @@ class SciServerJobRunner:
 
     @staticmethod
     def run_preprocessing_pipeline(**kwargs):
+        # TODO: change from kwargs
         """Run the preprocessing pipeline in science-server"""
 
-        results_path = kwargs.get('results_path',
-                                  '/home/idies/workspace/Storage/{}/AstroResearch'
-                                  .format(os.environ.get('SCISERVER_USERNAME')))
+        results_path = kwargs.get('results_path')
+        code_tar_location = kwargs.get('code_tar_location')
 
         compute_domain = sj.getDockerComputeDomainFromName(SciServerJobRunner.JOBS_DOMAIN)
 
@@ -92,14 +94,50 @@ class SciServerJobRunner:
                                                       {'name': 'scratch', 'rootVolumeName': 'Temporary',
                                                        'needsWriteAccess': True}],
                                          resultsFolderPath=results_path)
-        sj.waitForJob(jobid, verbose=True)
+        if kwargs.get('wait_for_completion'):
+            sj.waitForJob(jobid, verbose=True)
+
+
+@click.command('Submit preprocessing job')
+@click.option('-u', '--sciserver-username',
+              default=os.environ['SCISERVER_USERNAME'],
+              type=str,
+              help='The username for sciserver',
+              show_default=True)
+@click.option('-p', '--sciserver-password',
+              default=os.environ['SCISERVER_PASSWORD'],
+              type=str,
+              help='User password for sciserver',
+              show_default=True)
+@click.option('-J', '--jobs-domain',
+              default='Small Jobs Domain',
+              type=click.Choice(['Small Jobs Domain', 'Large Jobs Domain'], case_sensitive=True),
+              help='Jobs domain for the preprocessing job',
+              show_default=True)
+@click.option('-V', '--user-volume',
+              default='AstroResearch',
+              type=str,
+              help='The sciserver-files volume to upload the code to (Should be in Storage volume pool)'
+                   'The volume will be created if it does not exist',
+              show_default=True)
+@click.option('-w', '--wait-for-completion',
+              is_flag=True,
+              help='If true, wait for preprocessing job to complete')
+def main(**kwargs):
+    uname = kwargs.get('sciserver_username')
+    passwd = kwargs.get('sciserver_password')
+    jobs_domain = kwargs.get('jobs_domain')
+    docker_image = 'SciServer Essentials'
+    user_volume = kwargs.get('user_volume')
+    wait_for_completion = kwargs.get('wait_for_completion')
+    SciServerJobRunner.login_sciserver(uname, passwd)
+    SciServerJobRunner.set_job_config(jobs_domain, docker_image)
+    code_tar_location = SciServerJobRunner.upload_repo('FileServiceJHU',
+                                                       volume_name=user_volume)
+    SciServerJobRunner.run_preprocessing_pipeline(code_tar_location=code_tar_location,
+                                                  results_path=f'/home/idies/workspace/Storage/{uname}/{user_volume}',
+                                                  wait_for_completion=wait_for_completion)
 
 
 if __name__ == '__main__':
-    SciServerJobRunner.login_sciserver(os.environ['SCISERVER_USERNAME'], os.environ['SCISERVER_PASSWORD'])
-    SciServerJobRunner.set_job_config('Small Jobs Domain', 'SciServer Essentials')
-    code_tar_location = SciServerJobRunner.upload_repo('FileServiceJHU',
-                                                       volume_name='AstroResearch')
-    SciServerJobRunner.run_preprocessing_pipeline(code_tar_location=code_tar_location,
-                                                  results_path='/home/idies/workspace/Storage/{}/AstroResearch'
-                                                  .format(os.environ['SCISERVER_USERNAME']))
+    main()
